e4803e2fdcd96374932507bda90eb652d79cc89b
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-11 13:38:37 +0530

    hi bye message in JSON format

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+export type HiByeJson = {
+  message: string;
+};
+
 class HiBye {
   hi(): string {
     return 'hi';
   }
+
   bye(): string {
     return 'bye';
+  }
+
+  hiJson(): HiByeJson {
+    return { message: 'hi' };
+  }
+
+  byeJson(): HiByeJson {
+    return { message: 'bye' };
   }
 }
 
--- test/unit/src/index.unit.test.ts
+++ test/unit/src/index.unit.test.ts
@@ -1,4 +1,4 @@
-import HiBye from '../../../src';
+import HiBye, { HiByeJson } from '../../../src';
 
 describe('Testing HiBye class', () => {
   let hiBye: HiBye;
@@ -13,5 +13,15 @@ describe('Testing HiBye class', () => {
 
   it('Should be able to call bye method', () => {
     expect(hiBye.bye()).toBe('bye');
+  });
+
+  it('Should be able to call hiJson method', () => {
+    const json: HiByeJson = hiBye.hiJson();
+    expect(json).toStrictEqual({ message: 'hi' });
+  });
+
+  it('Should be able to call byeJson method', () => {
+    const json: HiByeJson = hiBye.byeJson();
+    expect(json).toStrictEqual({ message: 'bye' });
   });
 });
