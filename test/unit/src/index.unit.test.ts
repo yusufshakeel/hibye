cd4f0bb2ea55a00658cb032064180aba560eca0d
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-11 13:10:13 +0530

    add test

---
+++ test/unit/src/index.unit.test.ts
@@ -0,0 +1,17 @@
+import { HiBye } from '../../../src';
+
+describe('Testing HiBye class', () => {
+  let hiBye: HiBye;
+
+  beforeAll(() => {
+    hiBye = new HiBye();
+  });
+
+  it('Should be able to call hi method', () => {
+    expect(hiBye.hi()).toBe('hi');
+  });
+
+  it('Should be able to call bye method', () => {
+    expect(hiBye.bye()).toBe('bye');
+  });
+});
